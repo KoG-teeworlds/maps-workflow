e720ae868acfe797bc9865cbb18549e8c7e09731
NootNoot <116788065+al3x707g@users.noreply.github.com>
2025-07-21 00:00:08 +0200

    fix twgpu setup, install external mapres

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,24 @@
-FROM python:3.12-slim
+FROM ghcr.io/astral-sh/uv:python3.12-bookworm-slim
 LABEL authors="NootNoot"
 
 WORKDIR /app
 
-COPY . .
+RUN apt-get update && apt-get install curl jq -y
 
-RUN chmod +x install.sh
+# install rust
+RUN curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+# install twgpu dependencies
+RUN apt-get install -y build-essential cmake pkg-config libclang-dev clang \
+    llvm-dev libavcodec-dev libavutil-dev libavformat-dev libavfilter-dev \
+    libavdevice-dev
+
+RUN . "/root/.cargo/env" && cargo install twgpu-tools
+
+COPY . .
+COPY --chmod=755 install.sh entrypoint.sh ./
+
 RUN ./install.sh --docker
 
-ENTRYPOINT ["python", "maps_workflow/main.py"]
+ENTRYPOINT ["./entrypoint.sh"]
--- install.sh
+++ install.sh
@@ -1,25 +1,57 @@
 #!/bin/sh
 
-if ! command -v python3 >/dev/null 2>&1; then
-  echo "Error: Python is required. Please install it and retry."
-  exit 1
-fi
+API_URL="https://api.github.com/repos/ddnet/ddnet/contents/data/mapres?ref=master"
+MAPRES_DIR="/usr/share/games/ddnet/data/mapres"
 
-PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+set -e
 
-REQUIRED_VERSION="3.12"
+if ! command -v uv >/dev/null 2>&1; then
+  echo "uv not found. Attempting to install."
 
-if [ "$(printf '%s\n' "$PYTHON_VERSION" "$REQUIRED_VERSION" | sort -V | head -n1)" != "$REQUIRED_VERSION" ]; then
-    echo "Error: Python $REQUIRED_VERSION or higher required. Found Python $PYTHON_VERSION."
+  if ! command -v curl >/dev/null 2>&1; then
+    echo "curl is required to install uv. Please install it and try again."
     exit 1
+  fi
+
+  echo "Installing uv using curl..."
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+
+  . $HOME/.local/bin/env
 fi
 
 if [ "$1" = "--docker" ]; then
-  pip install .
+  uv pip install --system .
 else
   if [ ! -d ".venv" ]; then
-    python3 -m venv .venv
+    uv venv
   fi
 
-  ./.venv/bin/pip install -e .
+  . .venv/bin/activate
+  uv pip install -e .
 fi
+
+if [ -d /usr/share/games/ddnet/data/mapres ]; then
+  echo "mapres directory already exists. Skipping download of external mapres."
+  exit 0
+fi
+
+if ! command -v jq >/dev/null 2>&1; then
+  echo "jq not found. Skipping download of external mapres."
+  exit 0
+fi
+
+if ! mkdir -p "$MAPRES_DIR" 2>/dev/null; then
+  echo "Failed to create directory $MAPRES_DIR. Try again with elevated privileges."
+  echo "Skipping download of external mapres."
+  exit 0
+fi
+
+json=$(curl -s "$API_URL")
+
+echo "$json" | jq -r '.[].download_url' | while read -r url; do
+  filename=$(basename "$url")
+  echo "Downloading $filename to $MAPRES_DIR."
+  if ! curl -Lf -o "$MAPRES_DIR/$filename" "$url"; then
+    echo "Failed to download $filename."
+  fi
+done
